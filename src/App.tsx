feaa34d4703b309c3946e1f9eb7c63a9691f5afa
MalshanPerera <malshanperera3@gmail.com>
2022-07-14 21:41:59 +0530

    Create Pages and Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Login from './pages/login/Login';
+import New from './pages/new/New';
+import Single from './pages/single/Single';
+
 const App = () => {
   return (
     <div className='App'>
-      <h1>Admin Panel</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home />}></Route>
+            <Route path='login' element={<Login />}></Route>
+            <Route path='users'>
+              <Route index element={<List />}></Route>
+              <Route path=':userId' element={<Single />}></Route>
+              <Route path='new' element={<New />}></Route>
+            </Route>
+            <Route path='products'>
+              <Route index element={<List />}></Route>
+              <Route path=':productId' element={<Single />}></Route>
+              <Route path='new' element={<New />}></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
